c62299e82b079570ed0ac724c2f058a9057423e5
VitalyVoznyak <vitalok.vozik@gmail.com>
2023-05-11 22:06:09 +0300

    May 11 part 4

--- VitChat/app/src/main/java/com/example/vitchat/MainActivity.java
+++ VitChat/app/src/main/java/com/example/vitchat/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.vitchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private static final String TAG = "_MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = auth.getCurrentUser();
+                if(user == null){
+                    Log.d(TAG,"Nou Authorized");
+                }else {
+                    Log.d(TAG,"Authorized");
+                }
+            }
+        });
+        auth.createUserWithEmailAndPassword("vitalok.vozik@gmail.com","Spike1998")
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG,e.getMessage());
+                        Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+        auth.sendPasswordResetEmail("vitalok.vozik@gmail.com");
     }
 }
